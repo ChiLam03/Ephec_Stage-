3a2c471c34a3944e4409dc46f8585912fb2da150
Chi Lâm <HE304574@students.ephec.be>
2025-05-03 20:43:24 +0200

    ok

--- RDM_Codage/src/main.cpp
+++ RDM_Codage/src/main.cpp
@@ -3,7 +3,7 @@
 #include <Wire.h>     // pour l'oled hd44780
 #include <hd44780.h>  // pour l'oled hd44780
 #include <hd44780ioClass/hd44780_I2Cexp.h> // pour l'oled hd44780
-#include <AiEsp32RotaryEncoder.h> // pour l'encodeur rotatif, teste de simulation
+//#include <AiEsp32RotaryEncoder.h> // pour l'encodeur rotatif, teste de simulation
 #include <DHT.h> // pour le dht22
 #include <Adafruit_NeoPixel.h>
 
@@ -31,7 +31,7 @@ const int greenChannel = 1;
 const int blueChannel  = 2;
 
 // Paramètres PWM
-const int freq = 5000;      // Fréquence en Hz
+const int freq = 2000;      // Fréquence en Hz
 const int resolution = 8;   // Résolution (8 bits : valeurs de 0 à 255)
 
 // Initialisation de l'écran LCD & du DHT
@@ -39,7 +39,6 @@ hd44780_I2Cexp lcd(0x27);
 DHT dht(DHT_PIN, DHTTYPE);
 
 // Variables globales
-//int encoder_value = 0;
 int last_encodeur_value = 0;
 int last_capteur_value = 0;
 float poidsKg = 0;
@@ -47,45 +46,6 @@ int previousButtonState = HIGH; // bouton non appuyé initialement
 int flag_confirm = 0; // flag pour activer le traitement après confirmation depuis LabVIEW
 bool affichageMesures = false;
 unsigned long confirmationTime = 0;
-
-// Encodeur rotatif pour simuler la pression
-// #define ROTARY_ENCODER_A_PIN 32
-// #define ROTARY_ENCODER_B_PIN 33
-// #define ROTARY_ENCODER_BUTTON_PIN 35
-// #define ROTARY_ENCODER_VCC_PIN -1
-// #define ROTARY_ENCODER_STEPS 4
-
-// AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, 
-// ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
-
-// int last_value_encodeur = 0;
-// int lastValue = -1;
-
-// void rotary_onButtonClick() {
-//   static unsigned long lastTimePressed = 0;
-//   if (millis() - lastTimePressed < 500) {
-//     return;
-//   }
-//   lastTimePressed = millis();
-//   delay(100);
-// }
-
-// void rotary_loop() {
-//   if (rotaryEncoder.encoderChanged()) {
-//     int value = rotaryEncoder.readEncoder();
-//     if (value != lastValue) {
-//       lastValue = value;
-//       encoder_value = value; // Mise à jour de la variable globale
-//     }
-//   }
-//   if (rotaryEncoder.isEncoderButtonClicked()) {
-//     rotary_onButtonClick();
-//   }
-// }
-
-// void IRAM_ATTR readEncoderISR() {
-//   rotaryEncoder.readEncoder_ISR();
-// }
 
 void setup() {
   Serial.begin(115200);
@@ -99,9 +59,9 @@ void setup() {
     // Affichage initial : attente de l'appui bouton
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Attente appuie btn");
+    lcd.print(" Wait press BTN ");
     lcd.setCursor(0, 1);
-    lcd.print("Press btn->Send Data");
+    lcd.print("BTN--> Send DATA");
 
   // Configuration des broches
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -125,12 +85,6 @@ void setup() {
   ledcWrite(greenChannel, 30);
   ledcWrite(blueChannel, 30);
 
-  // rotaryEncoder.begin();
-  // rotaryEncoder.setup(readEncoderISR);
-  // bool circleValues = false;
-  // rotaryEncoder.setBoundaries(0, 4095, circleValues);
-  // rotaryEncoder.setAcceleration(300);
-
   // LED rouge allumée au départ
   digitalWrite(LED_RED_PIN, HIGH);
   digitalWrite(LED_GREEN_PIN, LOW);
@@ -152,7 +106,12 @@ void loop() {
   // Lecture capteurs
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
-  int pressureValue = 300; //analogRead(PRESSURE_PIN);  // Reviens au capteur réel si branché
+  int pressureValue = 3000; //analogRead(PRESSURE_PIN);  // Reviens au capteur réel si branché
+
+  // Hystérésis pour le capteur de pression
+  if (!(pressureValue >= last_capteur_value - 5 && pressureValue <= last_capteur_value + 5)) {
+    last_capteur_value = pressureValue;
+  }
 
   // Vérifie le capteur DHT22
   if (isnan(temperature) || isnan(humidity)) {
@@ -176,7 +135,7 @@ void loop() {
       // Affichage début de l’envoi
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print(">>>  Send Data  <<<");
+      lcd.print(">> Send Data <<");
       lcd.setCursor(0, 1);
       lcd.print("Vers LabVIEW...");
     } else {
@@ -187,43 +146,42 @@ void loop() {
       // Affichage arrêt
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print(">>>  STOP Data  <<<");
+      lcd.print(">> STOP Data <<");
       lcd.setCursor(0, 1);
-      lcd.print("Press btn->Send Data");
+      lcd.print(" Wait press BTN ");
     }
   }
   previousButtonState = currentButtonState;
   
 
   // Conversion pression → kg
-  if (pressureValue >= 11) {
-    poidsKg = 0.4028 * pressureValue + 17.57;
+  if (last_capteur_value >= 11) {
+    poidsKg = 0.4028 * last_capteur_value + 17.57;
   } else {
     poidsKg = 0;
   }
 
-  // LED RGB dynamique (si envoi actif)
 // LED RGB dynamique (si envoi actif)
 if (envoiActif) {
   // Gestion LED RGB
-  if (pressureValue <= 819) {
+  if (last_capteur_value <= 819) {
     ledcWrite(redChannel, 0); ledcWrite(greenChannel, 255); ledcWrite(blueChannel, 0);
-    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(0, 255, 0));
-  } else if (pressureValue <= 1638) {
+    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(0, 0, 255));
+  } else if (last_capteur_value <= 1638) {
     ledcWrite(redChannel, 127); ledcWrite(greenChannel, 255); ledcWrite(blueChannel, 0);
-    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(127, 255, 0));
-  } else if (pressureValue <= 2457) {
+    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(127, 0, 255));
+  } else if (last_capteur_value <= 2457) {
     ledcWrite(redChannel, 255); ledcWrite(greenChannel, 255); ledcWrite(blueChannel, 0);
-    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(255, 255, 0));
-  } else if (pressureValue <= 3276) {
+    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(255, 0, 255));
+  } else if (last_capteur_value <= 3276) {
     ledcWrite(redChannel, 255); ledcWrite(greenChannel, 128); ledcWrite(blueChannel, 0);
-    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(255, 128, 0));
-  } else if (pressureValue <= 3956) {
+    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(255, 0, 128));
+  } else if (last_capteur_value <= 3956) {
     ledcWrite(redChannel, 255); ledcWrite(greenChannel, 0); ledcWrite(blueChannel, 0);
     for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(255, 0, 0));
   } else {
     ledcWrite(redChannel, 120); ledcWrite(greenChannel, 0); ledcWrite(blueChannel, 255);
-    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(120, 0, 255));
+    for (int i = 0; i < NUM_PIXELS; i++) strip.setPixelColor(i, strip.Color(200, 255, 0));
   }
 
   strip.show();
@@ -233,8 +191,11 @@ if (envoiActif) {
   lcd.print("T:"); lcd.print(temperature, 1);
   lcd.print("C H:"); lcd.print(humidity, 1); lcd.print("%");
   lcd.setCursor(0, 1);
-  lcd.print("Kg:"); lcd.print(poidsKg, 1);
-  lcd.print(" ADC:"); lcd.print(pressureValue);
+  lcd.print(poidsKg, 1);  
+  lcd.print("Kg "); 
+  lcd.print(last_capteur_value);
+  lcd.print("ADC"); 
+
 
   // Envoi série
   Serial.print(temperature); 
@@ -244,7 +205,7 @@ if (envoiActif) {
   Serial.print(poidsKg); 
   Serial.println(";");
 
-  delay(500);  // Ce delay reste seulement ici
+  delay(200);  // Ce delay reste seulement ici
 } else {
   // Effet visuel stylé pendant l’attente
   rainbowCycle(5);  // Effet fluide et continu
